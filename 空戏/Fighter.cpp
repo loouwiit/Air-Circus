c326df8c4bc9dcc73a80a4c5d12d97fea00e2c54
lo <loouwiit@outlook.com>
2023-05-27 22:21:53 +0800

    修改三击行为

--- 空戏/Fighter.cpp
+++ 空戏/Fighter.cpp
@@ -403,7 +403,8 @@ void Fighter::compute(float delta_Time, int now_Time)
 		if (is_Key(Key::Back, now_Time, Type::Click, Type::Click, Type::Click) && now_Time > self_Next_Back_Time)
 		{
 			//Èý»÷
-			set_Velocity(abs(self_Velocity) * sf::Vector2f(self_Rotation_SinCos[1], self_Rotation_SinCos[0]));
+			if (abs(self_Rotation - atan2(get_Velocity().y, get_Velocity().x))<0.7) set_Velocity(-get_Velocity());
+			else set_Velocity(abs(self_Velocity) * sf::Vector2f(self_Rotation_SinCos[1], self_Rotation_SinCos[0]));
 			//printf("Fighter::compute: triple back\n");
 			self_Next_Back_Time = now_Time + self_Back_Time_Limit;
 		}
